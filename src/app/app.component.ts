0f91714b166af85af25263fd457b6cb47c5b16eb
Maksim Mishchenko <max75583@gmail.com>
2019-07-11 19:40:50 +0300

    Project is complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello, I am the First Angular Project</h1>',
+    styles: [`h1{
+        text-align: center;
+        color: chocolate;
+    }`]
+})
+export class AppComponent{}
